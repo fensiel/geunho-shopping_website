3cf206e2580c11d4b59b65bc03aea367b0f345e5
shin geun ho <shingeunho@shinui-MacBookAir.local>
2026-06-15 11:01:32 +0900

    Fix: 로그인 상태에 따른 로그인/로그아웃 버튼 조건부 렌더링 추가

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,56 +1,63 @@
-import React from 'react'
+import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
+// 1. props로 authenticate와 setAuthenticate를 받아옵니다.
+const Navbar = ({ authenticate, setAuthenticate }) => {
+    const menuList = ['여성', 'Divided', '남성', '신생아/유아', '아동', 'Home', 'Sale', '지속가능성'];
+    const navigate = useNavigate();
 
-const Navbar = () => {
-        const menuList  =   ['여성','Divided','남성','신생아/유아','아동','H&M Home','Sale','지속가능성']
+    // 로그인 페이지로 이동하는 함수
+    const goToLogin = () => {
+        navigate("/login");
+    };
 
-        const navigate = useNavigate()
-        const goToLogin=()=>{
-            navigate("/login")
-        };
-        const search= (event)=>{
-            if(event.key === "Enter"){
-                // console.log("we click this key",event.key);
-                // 입력한 검색어를 읽어와서
-                let keyword = event.target.value
-                console.log("keyword",keyword);
-                // url을 바꿔준다
-                navigate(`/?q=${keyword}`);
-            }
+    // ✨ 로그아웃 처리 함수 추가
+    const handleLogout = () => {
+        setAuthenticate(false); // 로그인 상태를 false로 변경
+        navigate("/"); // 로그아웃 후 메인 페이지로 이동
+    };
+
+    const search = (event) => {
+        if (event.key === "Enter") {
+            let keyword = event.target.value;
+            console.log("keyword", keyword);
+            navigate(`/?q=${keyword}`);
         }
+    };
+
     return (
-    <div>
+        <div>
             <div>
-                <div class="login-button" onClick={goToLogin}>
-                    <FontAwesomeIcon icon={faUser}/>
-                    <div>로그인</div>
-                </div>
+                {/* ✨ 2. authenticate 상태에 따라 로그인/로그아웃 버튼을 다르게 보여줍니다 (삼항연산자) */}
+                {authenticate ? (
+                    <div className="login-button" onClick={handleLogout}>
+                        <FontAwesomeIcon icon={faUser} />
+                        <div>로그아웃</div>
+                    </div>
+                ) : (
+                    <div className="login-button" onClick={goToLogin}>
+                        <FontAwesomeIcon icon={faUser} />
+                        <div>로그인</div>
+                    </div>
+                )}
             </div>
 
-           {/* <div className='nav-section'>
-                <img width={100} 
-                src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UNIQLO_logo.svg/250px-UNIQLO_logo.svg.png'/>
-           </div> */}
-
-           <div className='menu-area'>
-               
-                    <ul className='menu-list'>
-                        {menuList.map(menu=>(
-                            <li>{menu}</li>
-                        ))}
-                    </ul>
+            <div className='menu-area'>
+                <ul className='menu-list'>
+                    {menuList.map((menu, index) => (
+                        <li key={index}>{menu}</li>
+                    ))}
+                </ul>
                
                 <div>
-                        <FontAwesomeIcon icon={faSearch}/>
-                        <input type='text' onKeyPress={(event)=>search(event)}/>
+                    <FontAwesomeIcon icon={faSearch} />
+                    <input type='text' onKeyPress={(event) => search(event)} />
                 </div>
-           </div>
+            </div>
         </div>
-   
     );
 };
 
-export default Navbar
+export default Navbar;
